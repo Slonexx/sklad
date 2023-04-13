3b8f28023587add7cc4344aaab0af8d1243def2a
Slonexxx <21moriarty21@mail.ru>
2023-04-13 17:14:47 +0600

    new

--- app/Http/Controllers/Setting/CreateAuthTokenController.php
+++ app/Http/Controllers/Setting/CreateAuthTokenController.php
@@ -66,12 +66,12 @@ class CreateAuthTokenController extends Controller
             ]);
         }
 
-        /*$cfg = new cfg();
+        $cfg = new cfg();
         $app = AppInstanceContoller::loadApp($cfg->appId, $accountId);
         $app->status = AppInstanceContoller::ACTIVATED;
         $vendorAPI = new VendorApiController();
         $vendorAPI->updateAppStatus($cfg->appId, $accountId, $app->getStatusName());
-        $app->persist();*/
+        $app->persist();
 
         return to_route('getKassa', ['accountId' => $accountId, 'isAdmin' => $request->isAdmin]);
     }
