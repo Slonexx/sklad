9301279220f618b8877104cd401b7810f44377f7
Slonexxx <21moriarty21@mail.ru>
2023-12-29 20:25:45 +0600

    fix

--- app/Services/ticket/TicketService.php
+++ app/Services/ticket/TicketService.php
@@ -78,7 +78,7 @@ class TicketService
                 ]);
             }
             if ($this->Setting->paymentDocument != null ){
-                $this->createPaymentDocument($this->Setting->paymentDocument, $entity_type, $put, $Body['payments']);
+                $this->createPaymentDocument($this->Setting, $entity_type, $put, $Body['payments']);
             }
 
             return response()->json([
@@ -301,9 +301,9 @@ class TicketService
 
     }
 
-    private function createPaymentDocument( string $paymentDocument, string $entity_type, mixed $OldBody, mixed $payments): void
+    private function createPaymentDocument( mixed $document, string $entity_type, mixed $OldBody, mixed $payments): void
     {
-        switch ($paymentDocument){
+        switch ($document->paymentDocument){
             case "1": {
                 $url = 'https://api.moysklad.ru/api/remap/1.2/entity/';
                 if ($entity_type != 'salesreturn') {
@@ -455,6 +455,84 @@ class TicketService
                         'rate' => $rate
                     ];
                     if ($body['rate'] == null) unlink($body['rate']);
+                    $this->msClient->post($url_to_body, $body);
+                }
+                break;
+            }
+            case "4":{
+                $url = 'https://api.moysklad.ru/api/remap/1.2/entity/';
+                $url_to_body = null;
+                foreach ($payments as $item){
+                    $change = 0;
+                    if ($item['payment_type'] == 0){
+                        if ($entity_type != 'salesreturn') {
+                            if ($document->OperationCash == 1) {
+                                $url_to_body = $url . 'cashin';
+                            }
+                            if ($document->OperationCash == 2) {
+                                $url_to_body = $url . 'paymentin';
+                            }
+                            if ($document->OperationCash == 0) {
+                                continue;
+                            }
+                        }
+                        if (isset($item['change'])) $change = $item['change'];
+                    }
+                    else {
+                        if ($entity_type != 'salesreturn') {
+                            if ($document->OperationCard == 1) {
+                                $url_to_body = $url . 'cashin';
+                            }
+                            if ($document->OperationCard == 2) {
+                                $url_to_body = $url . 'paymentin';
+                            }
+                            if ($document->OperationCard == 0) {
+                                continue;
+                            }
+                        }
+                    }
+
+                    $rate_body = $this->msClient->get("https://api.moysklad.ru/api/remap/1.2/entity/currency/")->rows;
+                    $rate = null;
+                    foreach ($rate_body as $item_rate){
+                        if ($item_rate->name == "тенге" or $item_rate->fullName == "Казахстанский тенге"){
+                            $rate =
+                                ['meta'=> [
+                                    'href' => $item_rate->meta->href,
+                                    'metadataHref' => $item_rate->meta->metadataHref,
+                                    'type' => $item_rate->meta->type,
+                                    'mediaType' => $item_rate->meta->mediaType,
+                                ],
+                                ];
+                        }
+                    }
+
+                    $body = [
+                        'organization' => [  'meta' => [
+                            'href' => $OldBody->organization->meta->href,
+                            'type' => $OldBody->organization->meta->type,
+                            'mediaType' => $OldBody->organization->meta->mediaType,
+                        ] ],
+                        'agent' => [ 'meta'=> [
+                            'href' => $OldBody->agent->meta->href,
+                            'type' => $OldBody->agent->meta->type,
+                            'mediaType' => $OldBody->agent->meta->mediaType,
+                        ] ],
+                        'sum' => ($item['total']-$change) * 100,
+                        'operations' => [
+                            0 => [
+                                'meta'=> [
+                                    'href' => $OldBody->meta->href,
+                                    'metadataHref' => $OldBody->meta->metadataHref,
+                                    'type' => $OldBody->meta->type,
+                                    'mediaType' => $OldBody->meta->mediaType,
+                                    'uuidHref' => $OldBody->meta->uuidHref,
+                                ],
+                                'linkedSum' => 0
+                            ], ],
+                        'rate' => $rate
+                    ];
+                    if ($body['rate'] == null) unset($body['rate']);
                     $this->msClient->post($url_to_body, $body);
                 }
                 break;
